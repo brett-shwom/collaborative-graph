a1b62f24d598dc4b30a41b4c9667eb7ff9f12f53
Brett Shwom <brett.shwom@nyu.edu>
2014-06-07 15:08:52 -0400

    build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,68 @@
 var gulp = require('gulp');
- 
-// Let's make things more readable by
-// encapsulating each part's setup
-// in its own method
+var stylus = require('gulp-stylus');
+
+var EXPRESS_PORT = 9000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+
 function startExpress() {
- 
+
   var express = require('express');
   var app = express();
-  app.use(express.static(__dirname));
-  app.listen(4000);
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
 }
- 
-gulp.task('default', function () {
- 
+
+function startLivereload() {
+
+  lr = require('tiny-lr')();
+  lr.listen(LIVERELOAD_PORT);
+}
+
+// Notifies livereload of changes detected
+// by `gulp.watch()`
+function notifyLivereload(event) {
+  // `gulp.watch()` events provide an absolute path
+  // so we need to make it relative to the server root
+  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+
+  lr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+function buildStylus(event) {
+
+  var fileName;
+
+  if (event) {
+    fileName = require('path').relative(EXPRESS_ROOT, event.path);
+  }
+  else {
+    fileName = '*.styl'
+  }
+
+  gulp.src(fileName)
+    .pipe(stylus())
+    .pipe(gulp.dest('./'));
+
+
+}
+
+gulp.task('build', function () {
+  buildStylus()
+});
+
+gulp.task('server', function () {
+
   startExpress();
+  startLivereload();
+  buildStylus();
+  gulp.watch('*.html', notifyLivereload);
+  gulp.watch('*.css', notifyLivereload);
+  gulp.watch('*.js', notifyLivereload);
+  gulp.watch('*.styl', buildStylus);
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+Collaborative Graph
+--------------------
+
+A collaborative graph for use at Bruce's presentation.
+
+Check it out at http://collaborative-graph.herokuapp.com/
+
+try `gulp server`
