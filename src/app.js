a2f19c74ac24d1072e0f3124161c8abc4dd71d9b
Brett Shwom <brett.shwom@nyu.edu>
2014-06-07 20:51:06 -0400

    now computing average and imported rickshaw

--- src/app.js
+++ src/app.js
@@ -1,7 +1,54 @@
-angular.module("CollaborativeGraph", ["firebase"])
+angular.module("CollaborativeGraph", ["firebase",'angular-rickshaw'])
   .factory("EstimateService", ["$firebase", function($firebase) {
     var ref = new Firebase("https://bruce-thing.firebaseio.com/estimates");
     return $firebase(ref);
+  }])
+  .factory('AngularFirebasePropertyFilter', function () {
+
+    var angularFirebaseProperties = ["$id", "$bind", "$add", "$save", "$set", "$update", "$transaction", "$remove", "$child", "$on", "$off", "$auth", "$getIndex", "$getRef"]
+
+    return {
+        notAnAngularFirebaseProperty : function(property) {
+            return angularFirebaseProperties.indexOf(property) === -1
+        }
+    }
+  })
+  .factory('ComputeAverageEstimateForEachDay', ["AngularFirebasePropertyFilter", function (AngularFirebasePropertyFilter) {
+
+        return function (estimates) {
+            var averages = {}
+
+            var totalEstimates =  Object.keys(estimates)
+                                    .filter(AngularFirebasePropertyFilter.notAnAngularFirebaseProperty)
+                                    .length
+
+            Object.keys(estimates)
+                    .filter(AngularFirebasePropertyFilter.notAnAngularFirebaseProperty) //there's gotta be some other way to do this
+                    .map(function (estimateSetKey) {
+                        return estimates[estimateSetKey]
+                    })
+                    .forEach(function(estimateSet) {
+
+                        Object.keys(estimateSet.forDays)
+                            .forEach(function (daysEstimate) {
+
+                                if (!(daysEstimate in averages)) { //initialize
+                                    averages[daysEstimate] = 0
+                                }
+
+                                averages[daysEstimate]+= +estimateSet.forDays[daysEstimate] === NaN ? 0 : +estimateSet.forDays[daysEstimate] //count bogus input as 0
+
+                            })
+
+                    })
+
+            Object.keys(averages)
+                .forEach(function(days) {
+                    averages[days] = averages[days] / totalEstimates
+                })
+
+            return averages
+        }
   }])
   .controller("InputController", ["$scope", "EstimateService",
     function($scope, estimateService) {
@@ -15,8 +62,32 @@ angular.module("CollaborativeGraph", ["firebase"])
       };
     }
   ])
-  .controller("GraphController", ["$scope", "EstimateService",
-    function($scope, estimateService) {
-      $scope.estimates = estimateService;
+  .controller("GraphController", ["$scope", "EstimateService","ComputeAverageEstimateForEachDay",
+    function($scope, estimateService, computeAverageEstimateForEachDay) {
+
+        function recomputeAverages() {
+            $scope.averages = computeAverageEstimateForEachDay($scope.estimates)
+            console.log($scope.averages)
+        }
+
+        $scope.graph = {
+            features : {},
+            series : [{
+                name : 'series1',
+                color : 'steelblue',
+                data : [{x:1,y:1},{x:10,y:10}]
+            }],
+            options : {
+                renderer : 'bar',
+                width : 100
+            }
+        }
+
+        $scope.estimates = estimateService;
+        $scope.averages = {}
+        $scope.estimates.$on('value', recomputeAverages) //initial data load
+        $scope.estimates.$on('child_added', recomputeAverages)
+        $scope.estimates.$on('child_removed', recomputeAverages)
+        $scope.estimates.$on('child_changed', recomputeAverages)
     }
   ]);
